0c68301a972ec9231a3814306b133a2ddc5e0554
Regedit-msc <efusanyaae@gmail.com>
2021-11-10 17:54:52 +0100

    minor fix

--- android/app/src/main/kotlin/com/regedit/webcomic/webcomic/MainActivity.kt
+++ android/app/src/main/kotlin/com/regedit/webcomic/webcomic/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.regedit.webcomic.webcomic
+import android.view.WindowManager.LayoutParams
+
+import io.flutter.embedding.android.FlutterActivity
+
+import io.flutter.embedding.engine.FlutterEngine
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
