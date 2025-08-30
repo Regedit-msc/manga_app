91c4721ab038228e2cba661204b65db30ef3d454
mide <efusanyaae@gmail.com>
2025-08-30 01:45:29 +0100

    Update iOS scheme configuration, API constants, and dependencies
    - Added custom LLDB init file to iOS Runner scheme for debugging.
    - Updated API endpoint in api_constants.dart to a new ngrok URL.
    - Modified pubspec.lock to downgrade several Firebase packages and update their checksums.
    - Updated pubspec.yaml to reflect the downgraded versions of Firebase packages and other dependencies.

--- lib/data/common/constants/api_constants.dart
+++ lib/data/common/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   ApiConstants._();
   static const String httpLink =
-      "https://0695-197-211-59-86.ngrok-free.app/graphql";
+      "https://3e6fc4cd7eb2.ngrok-free.app/graphql";
   static const String unSplashUrl =
       "https://api.unsplash.com/search/collections";
   static const String unSplashApiHeader =
